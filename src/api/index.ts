b5101729f3d584e1b32aebec633e4b4ca0c104b2
Zhang Qiang <qiang.zhang@garena.cn>
2020-01-17 18:14:52 +0800

    add types

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,13 +1,11 @@
-
-const collectionId = process.env.NODE_ENV === 'development' ? '128/api/' : '';
 const install = (Vue: any) => {
   // eslint-disable-next-line no-param-reassign
   Vue.prototype.$api = {
-    css: `${process.env.VUE_APP_API_URL + collectionId}css`,
-    userinfo: `${process.env.VUE_APP_API_URL + collectionId}userinfo`,
-    enter: `${process.env.VUE_APP_API_URL + collectionId}enter`,
-    fireup: `${process.env.VUE_APP_API_URL + collectionId}fireup`,
-    redeem: `${process.env.VUE_APP_API_URL + collectionId}redeem`,
+    css: 'css',
+    userinfo: 'userinfo',
+    enter: 'enter',
+    fireup: 'fireup',
+    redeem: 'redeem',
   };
 };
 
---
+++ types/http.d.ts
@@ -0,0 +1,7 @@
+import { AxiosPromise } from 'axios';
+
+// eslint-disable-next-line max-len
+export type HttpScheme = {
+    get:(url:string, params?:[string, object, number], ...args:any[]) => AxiosPromise<any>;
+    post:(url:string, params?:[string, object, number], ...args:any[]) => AxiosPromise<any>;
+}
