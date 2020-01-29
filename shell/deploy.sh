e36a8e07cf1f8f0538cb44d0c22d6955f51a0df4
harryzq <1562055324@qq.com>
2020-01-29 20:40:05 +0800

    add shell

---
+++ shell/deploy.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+# !!!!运行前请给予权限！！！ 执行下面的命令👇
+# chmod u+r+x deploy.sh
+
+# 本地打包测试
+function deploy_local(){
+    vue-cli-service build --mode development
+    echo "Deploy development envirment successfully!"
+}
+
+# 部署测试环境
+function deploy_test(){
+    vue-cli-service build --mode test
+    echo "Deploy development envirment successfully!"
+}
+
+# 部署正式环境
+function deploy_prod(){
+    vue-cli-service build --mode production
+    echo "Deploy development envirment successfully!"
+}
+
+
+function deploy(){
+    deploy_type="$1"
+    if [ $deploy_type == "dev" ];
+        then
+            deploy_local
+    elif [ $deploy_type == "test" ];
+        then
+            deploy_test
+    elif [ $deploy_type == "prod" ];
+        then
+            deploy_prod
+    else
+        echo "You must give the wrong argument, please check it and the allowed arguments are 'test, prod, dev'"
+    fi
+
+}
+
+deploy $@
--- src/uiFeatures/utils/utils.ts
+++ src/uiFeatures/utils/utils.ts
@@ -1,4 +1,4 @@
-export const isVNode = function(node:object,key:string){
-    const hasOwnProperty = Object.prototype.hasOwnProperty
-    return hasOwnProperty.call(node, key);
-}
+export const isVNode = function (node:object, key:string) {
+  const { hasOwnProperty } = Object.prototype;
+  return hasOwnProperty.call(node, key);
+};
